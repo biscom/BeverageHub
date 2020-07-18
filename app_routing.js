ce6baa2fcebe7ed8799f4a6d6b33b7e95581aefb
Michael Biscotti <biscom@rpi.edu>
2020-07-18 01:58:27 -0400

    Added API search functionality & modal skeleton

--- app_routing.js
+++ app_routing.js
@@ -7,20 +7,32 @@ var fs = require('fs');
 var json2csv = require('json2csv').parse;
 var json2xml = require('json2xml');
 var mkdirp = require('mkdirp');
-var Chart = require('chart.js');
 var moment = require('moment');
+var config = require('config');
 
 // For parsing post requests
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // json encoded bodies
 app.use(bodyParser.urlencoded({extended: false})); // encoded bodies
 
+// Node-Untappd API setup
+var UntappdClient = require("node-untappd");
+
+var debug = false;
+var untappd = new UntappdClient(debug);
+
+var clientId = config.get("clientId"); // CLIENT ID
+var clientSecret = config.get("clientSecret"); // CLIENT SECRET
+
+untappd.setClientId(clientId);
+untappd.setClientSecret(clientSecret);
+
 // Serve static files with express
 app.use(express.static('public'));
 
 /* MongoDB setup */
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/twitter-collection',{useNewUrlParser: true});
+mongoose.connect('mongodb://127.0.0.1:27017/untappd-collection',{useNewUrlParser: true});
 
 
 app.route('/')
@@ -32,5 +44,21 @@ app.route('/app')
     .get(function (req, res) {
         res.sendFile(__dirname + '/public' + '/app.html');
     });
+
+app.route('/app/apisearch')
+    .post(function (req, res) {
+        // q: A UTF-8, URL-encoded search query of 500 characters maximum, including operators.
+        var q = req.body.q;
+        // https://api.untappd.com/v4/search/beer
+
+// The user we want to lookup for this example
+    var data = {};
+data.q = q;
+untappd.beerSearch(function(err, obj) {
+    console.log(err, obj);
+    var beers = obj.response.beers
+    res.json(beers);
+}, data);
+});
 
 app.listen(3000, () => console.log('BevPricing listening on port 3000!'));
--- public/core.js
+++ public/core.js
@@ -1,4 +1,6 @@
 // Angular.js
+/*global console*/
+/* eslint no-console: "off" */
 
 (function (angular) {
     'use strict';
@@ -10,12 +12,46 @@
             // Allow same origin resource loads
             'self',
             // Allow loading from API
-            'https://api.twitter.com/**'
+            'https://api.untappd.com/**'
         ]);
     });
 
-	app.controller('AppController', ['$scope', '$http', '$interval', function ($scope, $http, $interval) {
+	app.controller('AppController', ['$scope', '$http', function ($scope, $http) {
+        $scope.beer_list = [];
+
+		// Model for search criteria
+        $scope.api_search = {
+            q: ''
+        };
         
+        
+        // Called when search form is submitted
+        $scope.submit = function (submit_type) {
+            // Start loading indicator
+            $scope.loading_message = 'Loading...';
+            console.log(submit_type);
+            /* Submit to appropriate endpoint */
+            if (submit_type === 'apisearch') {
+                // Format search query
+                var data = {
+                    q: $scope.apisearch.q
+                };
+
+                // Send request to backend
+                // Start loading results
+                $scope.loading_message = 'Loading...';
+                $http.post('/app/apisearch', data)
+                    .then(function onSuccess(response) {
+                        $scope.beers = response.data;
+                        console.log($scope.beers);
+                        $scope.loading_message = `${$scope.beers.count} results for ${data.q}!`;
+
+                    }, function onError(response) {
+                        $scope.loading_message = 'Failed to load!';
+
+                    });
+            }
+        };
     }]);
 
-})(window.angular);
+}(window.angular));
